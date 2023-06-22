f76ce1424b0b3fe0675fb88edc88b94f2fe8a28f
Kawsar <web.kawsarahmed@gmail.com>
2023-06-22 17:03:22 +0600

    minor change in session configuration

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: "*",
+    origin: FRONT_END_URL,
     credentials: true,
 }));
 
