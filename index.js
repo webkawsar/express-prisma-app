58c7be82ed4eb5a08fd744093570777ec1deb220
Kawsar <web.kawsarahmed@gmail.com>
2023-06-23 16:07:58 +0600

    minor change

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 2 * 60 * 100 * 1000,
-        sameSite: "lax",
+        sameSite: "none",
         httpOnly: false,
         secure: true,
         signed: true
