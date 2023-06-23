8f9c3cc0900a45114e3379a1a9b30509e64a7e42
Kawsar <web.kawsarahmed@gmail.com>
2023-06-23 21:28:56 +0600

    fix self update problem

--- middlewares/validators/userValidator.js
+++ middlewares/validators/userValidator.js
@@ -1,118 +1,120 @@
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 const prisma = require("../../lib/prisma");
 
 const createUserValidator = [
-    check('firstName')
-        .notEmpty()
-        .withMessage('First name is required')
-        .trim()
-        .isLength({ max: 20, min: 2 })
-        .withMessage('First name must be between 2 to 20 char'),
+  check("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .trim()
+    .isLength({ max: 20, min: 2 })
+    .withMessage("First name must be between 2 to 20 char"),
 
-    check('lastName')
-        .notEmpty()
-        .withMessage('Last name is required')
-        .trim()
-        .isLength({ max: 20, min: 2 })
-        .withMessage('Last name must be between 2 to 20 char'),
+  check("lastName")
+    .notEmpty()
+    .withMessage("Last name is required")
+    .trim()
+    .isLength({ max: 20, min: 2 })
+    .withMessage("Last name must be between 2 to 20 char"),
 
-    check('email')
-        .notEmpty()
-        .withMessage('Email is required')
-        .isEmail()
-        .withMessage('Please add valid email')
-        .trim(),
-    // .normalizeEmail()
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please add valid email")
+    .trim(),
+  // .normalizeEmail()
 
-    check('email').custom(async (email) => {
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
-        if (user) {
-            throw new Error('Email is already registered!');
-        } else {
-            return true;
-        }
-    })
+  check("email").custom(async (email) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      throw new Error("Email is already registered!");
+    } else {
+      return true;
+    }
+  }),
 ];
 
 const createUserValidationResult = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).send({ success: false, message: errors.array()[0].msg })
-    }
-    next();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .send({ success: false, message: errors.array()[0].msg });
+  }
+  next();
 };
 
 const updateUserValidator = [
-    check('firstName')
-        .notEmpty()
-        .withMessage('First name is required')
-        .trim()
-        .isLength({ max: 20, min: 2 })
-        .withMessage('First name must be between 2 to 20 char'),
+  check("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .trim()
+    .isLength({ max: 20, min: 2 })
+    .withMessage("First name must be between 2 to 20 char"),
 
-    check('lastName')
-        .notEmpty()
-        .withMessage('Last name is required')
-        .trim()
-        .isLength({ max: 20, min: 2 })
-        .withMessage('Last name must be between 2 to 20 char'),
+  check("lastName")
+    .notEmpty()
+    .withMessage("Last name is required")
+    .trim()
+    .isLength({ max: 20, min: 2 })
+    .withMessage("Last name must be between 2 to 20 char"),
 
-    check('email')
-        .notEmpty()
-        .withMessage('Email is required')
-        .isEmail()
-        .withMessage('Please add valid email')
-        .trim(),
-    // .normalizeEmail()
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please add valid email")
+    .trim(),
+  // .normalizeEmail()
 
-    check('email').custom(async (email, { req }) => {
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+  check("email").custom(async (email, { req }) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        if(!user) {
-            return true;
-        } else if(user) {
-
-            if((req.user?.role === "Admin" || req.user?.role === "Support") && req.user?.email === email) {
-                throw new Error("This email is associated with another account");
-
-            } else if((req.user?.role === "Admin" || req.user?.role === "Support") && user?.id !== Number(req.params?.userId)) {
-                throw new Error("This email is associated with another account");
-
-            } else if((req.user?.role === "Admin" || req.user?.role === "Support") && user?.id === Number(req.params?.userId)) {
-                return true;
-
-            } else if(req.user?.email === email) {
-                return true;
-            } else {
-                throw new Error("This email is associated with another account");
-            }
-
-        } else {
-            throw new Error("This email is associated with another account");
-        }
-    })
+    if (!user) {
+      return true;
+    } else if (user) {
+      if (
+        (req.user?.role === "Admin" || req.user?.role === "Support") &&
+        user?.id !== Number(req.params?.userId)
+      ) {
+        throw new Error("This email is associated with another account");
+      } else if (
+        (req.user?.role === "Admin" || req.user?.role === "Support") &&
+        user?.id === Number(req.params?.userId)
+      ) {
+        return true;
+      } else if (req.user?.email === email) {
+        return true;
+      } else {
+        throw new Error("This email is associated with another account");
+      }
+    } else {
+      throw new Error("This email is associated with another account");
+    }
+  }),
 ];
 
 const updateUserValidationResult = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).send({ success: false, message: errors.array()[0].msg })
-    }
-    next();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .send({ success: false, message: errors.array()[0].msg });
+  }
+  next();
 };
 
-
 module.exports = {
-    createUserValidator,
-    createUserValidationResult,
-    updateUserValidator,
-    updateUserValidationResult
-}
+  createUserValidator,
+  createUserValidationResult,
+  updateUserValidator,
+  updateUserValidationResult,
+};
